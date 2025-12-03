b7a70491e3e1923711c65186c60de230f628c33d
YannCrt <yanncretin75@gmail.com>
2025-12-03 13:16:32 +0100

    Add Profil_page and Update home_page

--- technicaltestazeoo/lib/views/home_page.dart
+++ technicaltestazeoo/lib/views/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:technicaltestazeoo/models/user.dart';
-import 'package:technicaltestazeoo/services/user_service.dart';
+import 'package:technicaltestazeoo/notifiers/user_id_notifier.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,56 +9,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final ValueNotifier<bool> isLoadedNotifier = ValueNotifier(false);
-  final ValueNotifier<List<User>> usersNotifier = ValueNotifier([]);
+  final TextEditingController controller = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-    getUsers(); // appel de la fonction getAllUsers() pour charger tous les utilisateurs.
-  }
-
-  getUsers() async {
-    var loadedUsers = await UserService()
-        .getAllUsers(); // appel de la méthode getAllUsers() du service UserService
-    if (loadedUsers != null) {
-      usersNotifier.value = loadedUsers;
-      isLoadedNotifier.value = true;
-    }
-  }
-
-  // dsl j'ai mal a la gorge, soit le noir soit le rouge normalement, je ne sais pas, mais je pense que c'est le rouge sinon teste tout.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Technical Test Azéoo')),
-      body: ValueListenableBuilder<bool>(
-        valueListenable: isLoadedNotifier,
-        builder: (context, isLoaded, child) {
-          if (!isLoaded) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              "Entrer l'id de l'utilisateur",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 16),
 
-          return ValueListenableBuilder<List<User>>(
-            valueListenable: usersNotifier,
-            builder: (context, users, child) {
-              return ListView.builder(
-                itemCount: users.length,
-                itemBuilder: (context, index) {
-                  final user = users[index];
-                  return ListTile(
-                    title: Text('{$user.first_name} ${user.last_name}'),
-                    subtitle: Text('$user.email'),
-                    leading: user.countryFlag != null
-                        ? Image.network(user.countryFlag!)
-                        : null,
-                    trailing: Icon(Icons.person),
-                  );
-                },
-              );
-            },
-          );
-        },
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                labelText: "ID de l'utilisateur",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                final id = controller.text.trim();
+                if (id.isEmpty) return;
+
+                final parsedId = int.parse(id);
+
+                userIdNotifier.value = parsedId;
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Utilisateur $parsedId Sauvegardé !")),
+                );
+              },
+              child: const Text("Sauvegarder"),
+            ),
+          ],
+        ),
       ),
     );
   }
